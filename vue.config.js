aff6c2533a3cc802ebb8ab821eee0482b582c8b5
anggafsahid <angga.fsahid@gmail.com>
2025-02-08 18:37:35 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+// vue.config.js
+module.exports = {
+  publicPath: '/', // This ensures the app loads from the root path and doesn't get misconfigured.
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://cms-headless-production.up.railway.app',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+};
